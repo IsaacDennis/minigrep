5c33b763ea3c5b0695e66d541dc16adf76a5e3d8
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-03-20 14:59:29 -0300

    Commit inicial

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+	println!("An error occurred while interpreting the arguments: {}", err);
+	process::exit(1);
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+	println!("An error occurred: {}", e);
+	process::exit(1);
+    }
+}
